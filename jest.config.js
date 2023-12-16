78e8a6a8074868fc204c4d128d151bcd8165f7c7
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2023-12-16 23:39:06 +0900

    chore: exampleのテストコードは不要なので削除

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  transform: {
-    '\\.jsx?$': 'babel-jest',
-  },
-  modulePaths: ['<rootDir>/src/'],
-  moduleFileExtensions: ['js', 'ts', 'jsx', 'tsx', 'json'],
-  verbose: true,
-  preset: 'ts-jest',
-}
