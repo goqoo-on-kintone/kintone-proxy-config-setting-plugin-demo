39c409e20c618fc54ca372b866210bcbbbad719f
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2023-12-17 00:25:44 +0900

    feat: 自動採番テストアプリの型定義とコード作成

---
+++ dts/auto-number-fields.d.ts
@@ -0,0 +1,14 @@
+declare namespace kintone.types {
+  interface AutoNumberFields {
+    連番: kintone.fieldTypes.Number;
+  }
+  interface SavedAutoNumberFields extends AutoNumberFields {
+    $id: kintone.fieldTypes.Id;
+    $revision: kintone.fieldTypes.Revision;
+    更新者: kintone.fieldTypes.Modifier;
+    作成者: kintone.fieldTypes.Creator;
+    レコード番号: kintone.fieldTypes.RecordNumber;
+    更新日時: kintone.fieldTypes.UpdatedTime;
+    作成日時: kintone.fieldTypes.CreatedTime;
+  }
+}
--- goqoo.config.types.ts
+++ goqoo.config.types.ts
@@ -3,6 +3,7 @@ import type { Config as _Config, Auth } from 'goqoo'
 export type Env = 'development' // | 'staging' | 'production'
 
 export type AppId = {
+  auto_number: number // 自動採番テスト
   project: number // 案件管理
   customer: number // 顧客管理
   sales_activity: number // 活動履歴
