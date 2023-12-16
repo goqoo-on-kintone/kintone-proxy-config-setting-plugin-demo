f1890d12e2865f3fcf9ce913b68ce6e77b15fd40
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2023-12-16 23:29:33 +0900

    style: 1行文字数を140に変更

--- src/apps/project/update-customer.ts
+++ src/apps/project/update-customer.ts
@@ -39,50 +39,47 @@ kintone.events.on(['app.record.create.show', 'app.record.edit.show'], (event: De
   return event
 })
 
-kintone.events.on(
-  ['app.record.create.submit.success', 'app.record.edit.submit.success'],
-  async (event: DetailEvent<ProjectRecord>) => {
-    const customerAppId = kintone.app.getLookupTargetAppId('顧客名')!
+kintone.events.on(['app.record.create.submit.success', 'app.record.edit.submit.success'], async (event: DetailEvent<ProjectRecord>) => {
+  const customerAppId = kintone.app.getLookupTargetAppId('顧客名')!
 
-    const {
-      顧客管理レコード番号_関連レコード紐付け用: { value: customerRecordId },
-      部署名: { value: deptName },
-      担当者名: { value: contactName },
-    } = event.record
+  const {
+    顧客管理レコード番号_関連レコード紐付け用: { value: customerRecordId },
+    部署名: { value: deptName },
+    担当者名: { value: contactName },
+  } = event.record
 
-    // 部署名・担当者名に変更があれば、顧客管理レコードを更新する
-    const query = `$id="${customerRecordId}"`
-    const params = `?app=${customerAppId}&query=${encodeURIComponent(query)}`
-    const customerRecord = await fetchViaPluginProxy<KintoneApiResponse<CustomerRecord>>({
-      pluginId: context.externalApi.proxyConfigPluginId,
-      url: context.externalApi.kintone.recordsGet.url + params,
-      method: context.externalApi.kintone.recordsGet.method,
-    }).then((res) => res.records[0])
+  // 部署名・担当者名に変更があれば、顧客管理レコードを更新する
+  const query = `$id="${customerRecordId}"`
+  const params = `?app=${customerAppId}&query=${encodeURIComponent(query)}`
+  const customerRecord = await fetchViaPluginProxy<KintoneApiResponse<CustomerRecord>>({
+    pluginId: context.externalApi.proxyConfigPluginId,
+    url: context.externalApi.kintone.recordsGet.url + params,
+    method: context.externalApi.kintone.recordsGet.method,
+  }).then((res) => res.records[0])
 
-    if (customerRecord.部署名.value !== deptName || customerRecord.担当者名.value !== contactName) {
-      try {
-        await fetchViaPluginProxy<KintoneApiResponse<CustomerRecord>>({
-          pluginId: context.externalApi.proxyConfigPluginId,
-          url: context.externalApi.kintone.recordPut.url,
-          method: context.externalApi.kintone.recordPut.method,
-          headers: { 'Content-Type': 'application/json' },
-          body: {
-            app: customerAppId,
-            id: customerRecord.$id.value,
-            record: {
-              部署名: { value: deptName },
-              担当者名: { value: contactName },
-            },
+  if (customerRecord.部署名.value !== deptName || customerRecord.担当者名.value !== contactName) {
+    try {
+      await fetchViaPluginProxy<KintoneApiResponse<CustomerRecord>>({
+        pluginId: context.externalApi.proxyConfigPluginId,
+        url: context.externalApi.kintone.recordPut.url,
+        method: context.externalApi.kintone.recordPut.method,
+        headers: { 'Content-Type': 'application/json' },
+        body: {
+          app: customerAppId,
+          id: customerRecord.$id.value,
+          record: {
+            部署名: { value: deptName },
+            担当者名: { value: contactName },
           },
-        })
-        alert('部署名または担当者名が変更されたので、顧客管理レコードも同時に更新しました。')
-      } catch (e) {
-        console.error(e)
-        alert(`部署名または担当者名が変更されましたが、顧客管理レコードの同時更新が失敗しました。
+        },
+      })
+      alert('部署名または担当者名が変更されたので、顧客管理レコードも同時に更新しました。')
+    } catch (e) {
+      console.error(e)
+      alert(`部署名または担当者名が変更されましたが、顧客管理レコードの同時更新が失敗しました。
 ※このレコード（案件管理）は問題なく更新されています。 `)
-      }
     }
-
-    return event
   }
-)
+
+  return event
+})
